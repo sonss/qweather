f702632635e411d6d831cdcc28cc38f796bacf61
sonss <peak@tuiqiao.me>
2018-07-15 10:50:17 +0800

    名称修改

--- app/src/main/java/cn/iqingdian/qweather/MainActivity.java
+++ app/src/main/java/cn/iqingdian/qweather/MainActivity.java
@@ -3,6 +3,8 @@ package cn.iqingdian.qweather;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.coolweather.android.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
